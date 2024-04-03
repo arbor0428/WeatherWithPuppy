dd03663fe8ad4c240063ee78675fc6e53eb17dff
Hannah <hannah1155@naver.com>
2024-04-03 22:27:10 +0900

    페이지 분리하기

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { fetchCityNames, fetchSearchResult } from '../api/airInfoApi';
-import SearchCity from './SearchCity/SearchCity';
-import SearchCityResult from './SearchCityResult/SearchCityResult';
-import Notfound from './NotFound'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
-    const [error, setError] = useState(null);
-    const [searchResult, setSearchResult] = useState(null);
-    const [cityNames, setCityNames] = useState([]);
-    const [selectedSido, setSelectedSido] = useState('서울'); // 기본값으로 '서울' 설정
-
-    useEffect(() => {
-        setError(null);
-
-        const fetchData = async () => {
-        try {
-            const cities = await fetchCityNames(selectedSido);
-            setCityNames(cities);
-        } catch (error) {
-            setError(error);
-        }
-        };
-
-        fetchData();
-    }, [selectedSido]);
-
-    const search = async (selectedCity) => {
-        try {
-        const result = await fetchSearchResult(selectedCity);
-            setSearchResult(result);
-        } catch (error) {
-            setError(error);
-            setSearchResult(null);
-        }
-    };
-
-    if (error) {
-        return <Notfound error={error} />; // 에러 페이지로 전환
-    }
 
     return (
-        <div className="App">
-            <h1>대기 오염 정보 검색</h1>
-            <SearchCity search={search} cityNames={cityNames} selectedSido={selectedSido} setSelectedSido={setSelectedSido} />
-            <SearchCityResult data={searchResult} />
-        </div>
+        <>
+            <div className="App">
+                어서오세요 산책날씨도우미입니다
+                <Link to='/SearchCity'>
+                    지역 검색하러가기
+                </Link>
+            </div>
+        </>
     );
 }
 
--- src/pages/SearchCity/SearchCity.jsx
+++ src/pages/SearchCity/SearchCity.jsx
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchCityNames, fetchSearchResult } from '../../api/airInfoApi';
+import SearchCityResult from '../SearchCityResult/SearchCityResult';
+import Notfound from '../NotFound';
 
 const sidos = ['서울', '부산', '대구', '인천', '광주', '대전', '울산', '경기', '강원', '충북', '충남', '전북', '전남', '경북', '경남', '제주', '세종'];
 
-function SearchCity({ search, cityNames, selectedSido, setSelectedSido }) {
+function SearchCity() {
+    const [error, setError] = useState(null);
+    const [searchResult, setSearchResult] = useState(null);
+    const [cityNames, setCityNames] = useState([]);
+    const [selectedSido, setSelectedSido] = useState('서울'); // 기본값으로 '서울' 설정
+    const [showSearchResult, setShowSearchResult] = useState(false); // 검색 결과를 보여줄지 여부
+
+    useEffect(() => {
+        setError(null);
+
+        const fetchData = async () => {
+        try {
+            const cities = await fetchCityNames(selectedSido);
+            setCityNames(cities);
+        } catch (error) {
+            setError(error);
+        }
+        };
+
+        fetchData();
+    }, [selectedSido]);
+
+    const search = async (selectedCity) => {
+        try {
+        const result = await fetchSearchResult(selectedCity);
+            setSearchResult(result);
+            setShowSearchResult(true); // 검색 결과를 보여주도록 설정
+        } catch (error) {
+            setError(error);
+            setSearchResult(null);
+            setShowSearchResult(false); // 검색 결과를 숨기도록 설정
+        }
+    };
+
+    if (error) {
+        return <Notfound error={error} />;
+    }
+    
+    
     const handleChangeSido = (event) => {
         setSelectedSido(event.target.value);
     };
@@ -13,6 +54,8 @@ function SearchCity({ search, cityNames, selectedSido, setSelectedSido }) {
     };
 
     return (
+        <>
+        <h1>대기 오염 정보 검색</h1>
         <form onSubmit={handleSearch}>
             <label htmlFor="sido">시/도 선택: </label>
             <select id="sido" value={selectedSido} onChange={handleChangeSido}>
@@ -28,6 +71,8 @@ function SearchCity({ search, cityNames, selectedSido, setSelectedSido }) {
             </select>
             <button type="submit">검색</button>
         </form>
+        {showSearchResult && <SearchCityResult data={searchResult} />}
+        </>
     );
 }
 
