ab61671b2f4d75a374f9ef1daad19593a65bffa4
Hannah <hannah1155@naver.com>
2024-04-05 17:49:51 +0900

    미세먼지 정도에 따른 body bgColor 변경추가

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Notfound() {
     return (
-        <div>
+        <div className='notfound'>
             <p>죄송합니다. 요청하신 결과를 찾을 수 없습니다.</p>
         </div>
     );
--- src/pages/SearchResult/SearchResult.jsx
+++ src/pages/SearchResult/SearchResult.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import { useLocation, Navigate, useParams } from 'react-router-dom';
 import GoodAirQuality from '../../components/GoodAirQuality';
 import BadAirQuality from '../../components/BadAirQuality';
@@ -10,13 +10,13 @@ import worstIcon from '../../assets/img/worst_icon.png';
 
 function getCategory(value) {
     if (value >= 0 && value <= 15) {
-        return { category: '좋아요', imageSrc: bestIcon };
+        return { category: '좋아요', imageSrc: bestIcon, backColor: '#96d2ff' };
     } else if (value >= 16 && value <= 35) {
-        return { category: '보통이에요', imageSrc: goodIcon };
+        return { category: '보통이에요', imageSrc: goodIcon, backColor: '#acdf9c' };
     } else if (value >= 36 && value <= 75) {
-        return { category: '나빠요', imageSrc: badIcon };
+        return { category: '나빠요', imageSrc: badIcon, backColor: '#ff8484'};
     } else if (value >= 76) {
-        return { category: '매우나뻐요', imageSrc: worstIcon };
+        return { category: '매우나뻐요', imageSrc: worstIcon, backColor: '#939393' };
     } else if (value === "-" || value === "" || value === undefined) {
         return { category: '정보없음', imageSrc: 'image-path-5' };
     }
@@ -26,12 +26,21 @@ function SearchResult() {
     const { cityName } = useParams();
     const location = useLocation();
     const searchData = location.state.searchData; // Optional chaining 사용
+    
+    const airQualityCategory = getCategory(searchData.pm10Value);
+    
+    useEffect(() => {
+        // body의 background 색상 설정
+        document.body.style.backgroundColor = airQualityCategory.backColor;
+        return () => {
+            // 컴포넌트가 언마운트될 때 초기 상태로 복원
+            document.body.style.backgroundColor = '';
+        };
+    }, [airQualityCategory.backColor]);
 
     if (!searchData) {
         return <Navigate to="/not-found" />; // 데이터가 없는 경우 NotFound 페이지로 리다이렉션
     }
-
-    const airQualityCategory = getCategory(searchData.pm10Value);
 
     return (
         <section className='searchresult'>
